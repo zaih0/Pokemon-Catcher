1a1e2bae81d9f229c5555e41cb09ee4eb69f4ff2
jelani <524024@vistacollege.nl>
2025-03-17 11:36:29 +0100

    begin gemaakt aan de nintendo ds

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nintendo DS</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <div class="ds">
+        <div class="screen"></div>
+        <div class="screen"></div>
+        <div class="buttons">
+            <div class="button"></div>
+            <div class="button"></div>
+        </div>
+    </div>
+</body>
+</html>
