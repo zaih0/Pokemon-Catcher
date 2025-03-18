c088a86a9d9a5c0c6d3e4a06289a093717129100
jelani <524024@vistacollege.nl>
2025-03-18 09:58:20 +0100

    3ds

--- index.php
+++ index.php
@@ -3,16 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nintendo DS</title>
+    <title>Nintendo 3DS</title>
     <link rel="stylesheet" href="index.css">
 </head>
 <body>
     <div class="ds">
-        <div class="screen"></div>
-        <div class="screen"></div>
+        <div class="top-screen"></div>
+        <div class="bottom-screen"></div>
         <div class="buttons">
+            <div class="circle-pad"></div>
             <div class="button"></div>
             <div class="button"></div>
+            <div class="button"></div>
+            <div class="button"></div>
+            <div class="home-button"></div>
+            <div class="start-button"></div>
+            <div class="select-button"></div>
         </div>
     </div>
 </body>
